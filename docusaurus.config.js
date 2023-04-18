5ce7eac116b1908dbfbed15b53c7b0c0481ee1bf
dunbby <capiche7@gmail.com>
2023-04-18 08:13:35 +0300

    making some docs structure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,11 +110,40 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Lotus Mu Wiki. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Lotus Mu Wiki.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'GOKI4JCJ1U',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'f48a2e1992f6a5c068a896a22f66c1c0',
+  
+        indexName: 'lotusmu',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Search placeholder [default: "Search docs"]
+        placeholder: 'Search for anything ...',
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
       },
     }),
 };
